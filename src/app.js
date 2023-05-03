8a8881b0d34b591dafcf84a4a1121be341fabb3a
meMaipal <dev.maipal@gmail.com>
2023-05-03 11:57:06 +0530

    correct routing using react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 import Dropdown from "./components/Dropdown";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Translate from "./components/Translate";
-// import Route from "./components/Route";
 import AccordionList from "./components/AccordionList";
 
 const items = [
@@ -32,7 +31,7 @@ function App() {
     const [selected, setSelected] = useState(options[0]);
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <HashRouter>
                 <Header />
                 <Routes>
                     <Route path="/" element={<AccordionList items={items} />} />
@@ -50,7 +49,7 @@ function App() {
                     />
                     <Route path="/translate" element={<Translate />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
